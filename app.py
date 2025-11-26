49a371ff154bdf7c13245574b7e912720e8acc36
Ayazlatif35 <ayazlatif35@gmail.com>
2025-11-26 17:56:56 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import streamlit_authenticator as stauth
-st.write("Version:", stauth.__version__)
+streamlit run check_version.py
 
 import streamlit as st
 st.set_page_config(page_title="Secure Name Search", layout="centered")
